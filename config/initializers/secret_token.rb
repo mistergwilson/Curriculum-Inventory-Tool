c51fb84e136b28bca94b00aa77f6cab437f4b8d7
Gregory Wilson <mistergwilson@gmail.com>
2013-03-15 17:35:55 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cardapp::Application.config.secret_token = 'bb83c2aaa0542bf7a7bf43d127558bf05da236fec9d8b98225dccca917e714720abc0edd1391f7dab19ed104d66191ead74c4482aee49ed5fe2518019f057040'
