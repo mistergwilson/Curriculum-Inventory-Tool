9dd84ce57357ba4207daf1972e8167a0ae5f322c
Gregory Wilson <mistergwilson@gmail.com>
2013-03-18 19:51:48 -0400

    Add Paperclip Gem

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,8 +1,12 @@
 class Card < ActiveRecord::Base
-  attr_accessible :title
+  attr_accessible :title, :image
+
 
   validates :title, presence: true
-
-  belongs_to :user
   validates :user_id, presence: true
+  validates_attachment :image, presence: true,
+ 								content_type: { content_type: ['image/jpeg', 'image/jpg' 'image/png', 'image/gif']},
+ 								size: { less_than: 7.megabytes }
+  belongs_to :user
+  has_attached_file :image, styles: { medium: "320x240>"}
 end
